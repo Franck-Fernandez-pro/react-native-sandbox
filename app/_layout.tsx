a8ed06c4dcc74951943b4f0c1b2f186e0b885daa
Franck Fernandez <franck.fernandez@gmail.com>
2023-10-17 11:02:31 +0200

    Fix issue on redirect before app loaded

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,13 +6,13 @@ export const unstable_settings = {
 };
 
 export default function Layout() {
-  // const [fontsLoaded] = useFonts({
-  //   DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-  //   DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-  //   DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-  // });
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
 
-  // if (!fontsLoaded) return null;
+  if (!fontsLoaded) return null;
   return (
     <Stack initialRouteName="home">
       <Stack.Screen name="home" />
